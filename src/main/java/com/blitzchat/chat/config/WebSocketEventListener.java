c8ae4ec32bcefee5a3a8f557fa55b8b55d302892
Edward Marzal <edsanchezmarzal@gmail.com>
2023-06-18 01:44:41 -0500

    Event for complete

--- src/main/java/com/blitzchat/chat/config/WebSocketEventListener.java
+++ src/main/java/com/blitzchat/chat/config/WebSocketEventListener.java
@@ -1,8 +1,20 @@
 package com.blitzchat.chat.config;
 
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class WebSocketEventListener {
 
+    @EventListener
+    public void HandleWebSocketDisconnectListener(
+            SessionDisconnectEvent event) {
+        // here need to complete it.
+    }
 }
